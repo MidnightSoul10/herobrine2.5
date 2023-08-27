26b84d1d0b12cac6c4753890ee9346fb033ca26b
FNF-Porter-Apk-Android <143233754+FNF-Porter-Apk-Android@users.noreply.github.com>
2023-08-27 10:38:35 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2251,7 +2251,7 @@ class PlayState extends MusicBeatState
 	public function updateScore(miss:Bool = false)
 	{
 		scoreTxt.text = 'Score: ' + songScore
-		+ ' | port by glauber01 | Misses: ' + songMisses
+		+ ' | Misses: ' + songMisses
 		+ ' | Rating: ' + ratingName
 		+ (ratingName != '?' ? ' (${Highscore.floorDecimal(ratingPercent * 100, 2)}%) - $ratingFC' : '');
 
