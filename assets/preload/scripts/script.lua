99feb04f6db7dedccc2b0a0f8955aeaca13fb00d
FNF-Porter-Apk-Android <143233754+FNF-Porter-Apk-Android@users.noreply.github.com>
2023-08-28 08:43:18 -0300

    Add files via upload

---
+++ assets/preload/scripts/script.lua
@@ -0,0 +1,20 @@
+currentDifficulty = 'its still fucked';
+
+function onCreate()
+    makeLuaText('songText', "Vs. Herobrine Reborn Tales 2.5 Optimized By The RedBlack Master", 0, 2, 700);
+    setTextAlignment('songText', 'left');
+    setTextSize('songText', 18);
+    setTextBorder('songText', 1, '2b0000');
+    setObjectCamera('songText', 'other');
+    addLuaText('songText');
+end
+
+function onCreatePost()
+    setProperty('timeBar.y', getProperty('timeBar.y') - 10);
+    setProperty('timeTxt.y', getProperty('timeTxt.y') - 10);
+end
+
+function onUpdate(elapsed)
+    currentDifficulty = getProperty('storyDifficultyText');
+    setTextString('songText', "Vs. Herobrine Reborn Tales 2.5 Optimized By The RedBlack Master");
+end
