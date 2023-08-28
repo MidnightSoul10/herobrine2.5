a0c3830c41fbde92fb05e205cadd265d883b8f20
FNF-Porter-Apk-Android <143233754+FNF-Porter-Apk-Android@users.noreply.github.com>
2023-08-28 09:20:00 -0300

    Update script.lua

--- assets/preload/scripts/script.lua
+++ assets/preload/scripts/script.lua
@@ -1,7 +1,7 @@
 currentDifficulty = 'its still fucked';
 
 function onCreate()
-    makeLuaText('songText', "Vs. Herobrine Reborn Tales 2.5 Optimized By The RedBlack Master", 0, 2, 700);
+    makeLuaText('songText', "Vs. Herobrine Reborn Tales 2.5 Optimized By VGPH Mods Port By Glauber01", 0, 2, 700);
     setTextAlignment('songText', 'left');
     setTextSize('songText', 18);
     setTextBorder('songText', 1, '2b0000');
