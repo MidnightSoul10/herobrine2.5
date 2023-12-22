154f39bd13c18b69e810e3359e7daea5fa221c52
MidnightSoul10 <111463416+MidnightSoul10@users.noreply.github.com>
2023-12-22 12:57:33 -0300

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -81,9 +81,6 @@ class CreditsState extends MusicBeatState
 		#end
 
 		var pisspoop:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color 
-			['Android Port'],
-			['glauber01', 'glauber01', 'Hi I ported this mod', 'https://youtube.com/@glauber01', 'FFFFFF'],
-			[''],
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',								'https://twitter.com/Shadow_Mario_',	'444444'],
 			['RiverOaken',			'river',			'Main Artist/Animator of Psych Engine',							'https://twitter.com/RiverOaken',		'B42F71'],
